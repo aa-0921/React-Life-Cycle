9d08b26c11363e70ab6dffd5ca9a981f356ced3d
aoyama <74999140+akuto-aoyama@users.noreply.github.com>
2021-07-06 11:50:28 +0900

    [add]ベース作成

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyComponent from './App.js';
+
+
+ReactDOM.render(
+    <MyComponent />,
+    document.getElementById('root')
+);
